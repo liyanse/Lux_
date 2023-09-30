a79a324693cf7a1c87b34e0ad3561e3f9c47431a
Lians <91662935+liyanse@users.noreply.github.com>
2023-09-30 12:06:17 +0300

    Lians&Lux

---
+++ forloop.py
@@ -0,0 +1,19 @@
+print("1st example")
+
+lst = [1, 2, 3]
+for i in range(len(lst)):
+     print(lst[i], end = " \n")
+ 
+print("2nd example")
+
+for j in range(0,5):
+    print(j, end = " \n")
+
+
+
+"""
+For loop Question: 
+Write a Python program that iterates the integers from 1 to 50.
+ For multiples of three print "Fizz" instead of the number and for multiples of five print "Buzz".
+ For numbers that are multiples of three and five, print "FizzBuzz".
+ """
---
+++ whileloop.py
@@ -0,0 +1,14 @@
+m = 5
+i = 0
+while i < m:
+     print(i, end = " ")
+     i = i + 1
+print("End")
+
+
+"""
+
+While loop Question: 
+Rock Paper Scissors
+
+ """
